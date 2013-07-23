48d143862cc718769ae46f758f9a529de7815dae
Nenad Samardzic <samardzicnenad@gmail.com>
2013-07-23 02:18:01 -0400

    Reconfiguration of the project repository

---
+++ DataReporter/grails-app/controllers/datareporter/CPUutilController.groovy
@@ -0,0 +1,184 @@
+/**********************************************************************
+ * Created by : Nenad Samardzic
+ * Date       : 07/15/2013
+ * Description: The class represents controller for CPU utilization data
+ * 				The class was originally created automatically
+ * 				Later, all of the functionalities that provided any kind of control except read-only
+ * 				actions were canceled
+ * 				After that, methods 
+ * 					- for creating dataset from the DB
+ * 					- for creating a chart
+ * 					- for taking a snapshot and
+ * 					- for putting all of this together were included in the class
+ **********************************************************************/
+package datareporter
+
+import org.springframework.dao.DataIntegrityViolationException
+
+import org.jfree.chart.JFreeChart
+import org.jfree.chart.axis.DateAxis
+import org.jfree.chart.axis.NumberAxis
+import org.jfree.chart.encoders.SunPNGEncoderAdapter
+import org.jfree.chart.ChartFactory
+import org.jfree.chart.ChartPanel
+import org.jfree.chart.plot.PlotOrientation
+import org.jfree.chart.plot.XYPlot
+import org.jfree.chart.renderer.xy.StandardXYItemRenderer
+import org.jfree.chart.renderer.xy.XYItemRenderer
+import org.jfree.data.category.DefaultCategoryDataset
+import org.jfree.data.general.DefaultValueDataset
+import org.jfree.data.general.ValueDataset
+import org.jfree.data.time.Minute
+import org.jfree.data.time.TimeSeries
+import org.jfree.data.time.TimeSeriesCollection
+import org.jfree.data.xy.XYDataset
+import org.jfree.ui.ApplicationFrame
+import org.jfree.ui.RefineryUtilities
+
+import java.awt.image.BufferedImage
+import java.awt.BasicStroke
+import java.awt.Color
+import java.awt.Dimension
+import java.text.SimpleDateFormat
+
+import groovy.sql.Sql
+
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
+
+class CPUutilController {
+
+	static allowedMethods = []
+
+	def index() {
+		redirect(action: "list", params: params)
+	}
+
+	def list(Integer max) {
+		params.max = Math.min(max ?: 50, 100)
+		[CPUutilInstanceList: CPUutil.list(params), CPUutilInstanceTotal: CPUutil.count()]
+	}
+
+	def show(Long id) {
+		def CPUutilInstance = CPUutil.get(id)
+		if (!CPUutilInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [message(code: 'CPUutil.label', default: 'CPUutil'), id])
+			redirect(action: "list")
+			return
+		}
+
+		[CPUutilInstance: CPUutilInstance]
+	}
+	
+	public static byte[] getChartAsImage(JFreeChart chart, int nWidth, int nHeight)
+	{
+		if(nWidth < 0 || nHeight < 0) //validate dimensions
+			throw new IllegalArgumentException("Chart dimension < 0")
+		// create PNG encoder and return byte array representation
+		SunPNGEncoderAdapter encoder = new SunPNGEncoderAdapter()
+		encoder.setEncodingAlpha(true)
+		def cb = encoder.encode(chart.createBufferedImage(nWidth, nHeight, BufferedImage.BITMASK, null))
+		return cb;
+	}
+	//creates chart with the provided dataset
+	private JFreeChart createChart(final XYDataset dataset) {
+		// create the chart...
+		final JFreeChart chart = ChartFactory.createTimeSeriesChart(
+				"CPU utilization chart", "Time", "Projected on a scale of 1", dataset, true, true, false)
+		//customize it
+		chart.setBackgroundPaint(Color.white)
+		final XYPlot plot = chart.getXYPlot()
+		plot.setBackgroundPaint(Color.lightGray)
+		plot.setDomainGridlinePaint(Color.white)
+		plot.setRangeGridlinePaint(Color.white)
+		// customize the plot
+		plot.setDomainCrosshairVisible(true)
+		plot.setRangeCrosshairVisible(false)
+		// customize the renderer...
+		final XYItemRenderer renderer = plot.getRenderer()
+		if (renderer instanceof StandardXYItemRenderer) {
+			final StandardXYItemRenderer rr = (StandardXYItemRenderer) renderer
+			rr.setPlotLines(true)
+			for (i in 0..8) renderer.setSeriesStroke(i, new BasicStroke(2.0f))
+		}
+		// customize date axis
+		final DateAxis axis = (DateAxis) plot.getDomainAxis();
+		axis.setDateFormatOverride(new SimpleDateFormat("YYY-MM-dd hh:mm"));
+		return chart;
+	}
+	//creates dataset (series) from the DB, accordingly to user-selected time interval
+	private XYDataset createDataset(String sInterval) {
+		int prevM = 0, prevH = 0, prevD = 0
+
+		def sql = Sql.newInstance("jdbc:mysql://localhost:3306/datagenerator",
+				ConfigurationHolder.getConfig().getProperty('dbUserName'), 
+				ConfigurationHolder.getConfig().getProperty('dbPassword'), "com.mysql.jdbc.Driver")
+		def cData = sql.dataSet("cpudata")
+		cData.sort { it.Time }
+
+		final TimeSeriesCollection dataset = new TimeSeriesCollection();
+		final TimeSeries s1 = new TimeSeries("User", Minute.class)
+		final TimeSeries s2 = new TimeSeries("System", Minute.class)
+		final TimeSeries s3 = new TimeSeries("Nice", Minute.class)
+		final TimeSeries s4 = new TimeSeries("Idle", Minute.class)
+		final TimeSeries s5 = new TimeSeries("Wait", Minute.class)
+		final TimeSeries s6 = new TimeSeries("Irq", Minute.class)
+		final TimeSeries s7 = new TimeSeries("SoftIrq", Minute.class)
+		final TimeSeries s8 = new TimeSeries("Stolen", Minute.class)
+		final TimeSeries s9 = new TimeSeries("Combined", Minute.class)
+		cData.each{ cpudata -> //Graph takes only the first element of the interval
+			if ((sInterval == "Minute" && //Dataset is sorted, so the one-way check is sufficient
+			(prevM != cpudata.Time.minutes ||
+			(prevM == cpudata.Time.minutes &&
+			(prevH != cpudata.Time.hours || prevD != cpudata.Time.date)))) ||
+			(sInterval == "Hour" &&
+			(prevH != cpudata.Time.hours ||
+			(prevH == cpudata.Time.hours && prevD != cpudata.Time.date))) ||
+			(sInterval == "Day" && prevD != cpudata.Time.date)) {
+				prevM = cpudata.Time.minutes
+				prevH = cpudata.Time.hours
+				prevD = cpudata.Time.date
+				s1.add(new Minute(cpudata.Time.minutes, cpudata.Time.hours,
+						cpudata.Time.date, cpudata.Time.month + 1, cpudata.Time.year +1900), cpudata.User);
+				s2.add(new Minute(cpudata.Time.minutes, cpudata.Time.hours,
+						cpudata.Time.date, cpudata.Time.month + 1, cpudata.Time.year +1900), cpudata.System);
+				s3.add(new Minute(cpudata.Time.minutes, cpudata.Time.hours,
+						cpudata.Time.date, cpudata.Time.month + 1, cpudata.Time.year +1900), cpudata.Nice);
+				s4.add(new Minute(cpudata.Time.minutes, cpudata.Time.hours,
+						cpudata.Time.date, cpudata.Time.month + 1, cpudata.Time.year +1900), cpudata.Idle);
+				s5.add(new Minute(cpudata.Time.minutes, cpudata.Time.hours,
+						cpudata.Time.date, cpudata.Time.month + 1, cpudata.Time.year +1900), cpudata.Wait);
+				s6.add(new Minute(cpudata.Time.minutes, cpudata.Time.hours,
+						cpudata.Time.date, cpudata.Time.month + 1, cpudata.Time.year +1900), cpudata.Irq);
+				s7.add(new Minute(cpudata.Time.minutes, cpudata.Time.hours,
+						cpudata.Time.date, cpudata.Time.month + 1, cpudata.Time.year +1900), cpudata.SoftIrq);
+				s8.add(new Minute(cpudata.Time.minutes, cpudata.Time.hours,
+						cpudata.Time.date, cpudata.Time.month + 1, cpudata.Time.year +1900), cpudata.Stolen);
+				s9.add(new Minute(cpudata.Time.minutes, cpudata.Time.hours,
+						cpudata.Time.date, cpudata.Time.month + 1, cpudata.Time.year +1900), cpudata.Combined);
+			}
+		}
+		dataset.addSeries(s1);
+		dataset.addSeries(s2);
+		dataset.addSeries(s3);
+		dataset.addSeries(s4);
+		dataset.addSeries(s5);
+		dataset.addSeries(s6);
+		dataset.addSeries(s7);
+		dataset.addSeries(s8);
+		dataset.addSeries(s9);
+		return dataset;
+	}
+
+	def drawChartImage(String sInterval) {
+		def chart = createChart(createDataset(sInterval))
+		return getChartAsImage(chart, 800, 600)
+	}
+	//initially set the graph to minutes
+	def returnCPUData = {
+		def sInterval = params.interval ? params.interval : "Minute"
+		def imgChartImage = drawChartImage(sInterval)
+		response.setContentType("image/png")
+		response.setContentLength(imgChartImage.length)
+		response.outputStream.write(imgChartImage)
+	}
+}
--- README.md
+++ README.md
@@ -8,4 +8,86 @@ This project contains two applications:
  - Grails/Groovy web application that displays graph of the data collected with a choice of intervals (per min, per hour, per day).
    Below the graph application shows the table with all of the data
    
-note: see Grails-Java-Sql-Homework.pdf for detailed info
+***************************
+USAGE INSTRUCTIONS:
+***************************
+
+
+DATA LAYER:
+***************************
+
+ - mySQL instalation required
+ - create database datagenerator
+ - create table cpudata
+ - create table netdata
+
+
+You can run this one script:
+
+delimiter ;
+CREATE DATABASE `datagenerator` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `datagenerator`;
+
+CREATE TABLE `cpudata` (
+  `idCPUData` int(11) NOT NULL AUTO_INCREMENT,
+  `Time` timestamp NULL DEFAULT NULL,
+  `User` double DEFAULT NULL,
+  `System` double DEFAULT NULL,
+  `Nice` double DEFAULT NULL,
+  `Idle` double DEFAULT NULL,
+  `Wait` double DEFAULT NULL,
+  `Irq` double DEFAULT NULL,
+  `SoftIrq` double DEFAULT NULL,
+  `Stolen` double DEFAULT NULL,
+  `Combined` double DEFAULT NULL,
+  PRIMARY KEY (`idCPUData`),
+  UNIQUE KEY `idCPUData_UNIQUE` (`idCPUData`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `netdata` (
+  `idNETData` int(11) NOT NULL AUTO_INCREMENT,
+  `Time` datetime DEFAULT NULL,
+  `InboundTCP` int(11) DEFAULT NULL,
+  `OutboundTCP` int(11) DEFAULT NULL,
+  `InboundAll` int(11) DEFAULT NULL,
+  `OutboundAll` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idNETData`),
+  UNIQUE KEY `idNETData_UNIQUE` (`idNETData`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+
+DATA GENERATOR APPLICATION:
+***************************
+
+DataGenerator is java 1.7 console application developed in Eclipse IDE. It uses SIGAR free tool which provides excellent results with system monitoring and collecting data.
+You need to perform the following steps:
+ - download DataGenerator folder and import it as Eclipse (Java) project
+ - from http://sourceforge.net/projects/sigar/files/ download SIGAR and unpack it
+ - in Java Build Path of the DataGenerator project add external JARs:
+   - mysql-connector-java-5.1.23-bin.jar (<installation path>\Connector J 5.1.23\)
+   - sigar.jar (\sigar-bin\lib\)
+ - when starting application, you'll be asked to provide mySQL username, password and the time interval in which the data will be collected
+
+
+
+DATA REPORTER APPLICATION:
+***************************
+
+DataReporter is Grails/Groovy web application developed in Groovy/Grails Tool Suite (GGTS) IDE.
+You need to perform the following steps:
+ - download DataReporter folder and import it in GGTS as Grails project
+ - in Config.groovy file (lines 62 and 63), in conf section of the project, you need to set values for two variables - DB username and password...values are the same ones you entered when staring DataGenerator application
+ - run the following script on datagenerator DB:
+
+use datagenerator;
+grant all on datagenerator.* to grails@localhost identified by 'server';
+flush privileges;
+
+ - execute grails run-app from GGTS command prompt
+ - after a while you'll see the message:
+
+Server running. Browse to http://localhost:9090/DataReporter
+
+ - you can open the web application using provided address
